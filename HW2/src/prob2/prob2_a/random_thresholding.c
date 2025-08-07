bec8c4ba2bf709ff03f087a3382e6593802c04f1
Tejas Harishchandra Acharya <224756424+t-h-acharya@users.noreply.github.com>
2025-08-07 05:18:09 +0000

    Add random_thresholding.c

---
+++ HW2/src/prob2/prob2_a/random_thresholding.c
@@ -0,0 +1,37 @@
+#include "../../utils/hw2.h"
+#include <time.h>
+
+int main(int argc, char **argv) {
+    if (argc != 5) {
+        fprintf(stderr, "USAGE: ./fixed_thresholding filename height width channels");
+        exit(EXIT_FAILURE);
+    }
+
+    char *filename = argv[1];
+    int height = atoi(argv[2]);
+    int width = atoi(argv[3]);
+    int channels = atoi(argv[4]);
+
+    char out_filename[100];
+    sprintf(out_filename, "./data/output/random_thresholding.raw");
+
+    Image *image = read_image(filename, height, width, channels);
+    if (!image) {
+        exit(EXIT_FAILURE);
+    }
+    
+    srand(time(NULL));
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            for (int k = 0; k < channels; k++) {
+                image->data[i][j][k] = (image->data[i][j][k] < (rand() % MAX_INTENSITY)) ? MIN_INTENSITY : MAX_INTENSITY;
+            }
+        }
+    }
+
+    write_image(out_filename, image);
+    
+    free_image(image);
+
+    exit(EXIT_SUCCESS);
+}
